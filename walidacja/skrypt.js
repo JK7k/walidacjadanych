a42bcb1521696a5367e05bc12f70d4f9b64960d5
JK7k <97955908+JK7k@users.noreply.github.com>
2022-11-30 11:20:48 +0100

    Add files via upload

---
+++ walidacja/skrypt.js
@@ -0,0 +1,10 @@
+const formularz = document.querySelector("form");
+const imie=formularz.querySelector("input[name=name]");
+const email=formularz.querySelector("input[name=email]");
+
+formularz.addEventListener("submit", e=>{
+    e.preventDefault();
+    if(imie.value.length<=3){
+        alert("Pisarzu! Wpisałeś za krótkie imie");
+    }
+})
